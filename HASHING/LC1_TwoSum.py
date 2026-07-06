027a4a0d274f512dc8c07e714d83871544cb3494
Pranavi Chowdary07 <pranavikasthuri07@gmail.com>
2026-07-06 09:29:37 +0530

    Two Sum

--- HASHING/LC1_TwoSum.py
+++ HASHING/LC1_TwoSum.py
@@ -6,3 +6,9 @@ class Solution:
             if complement in seen:
                 return [seen[complement], i]
             seen[num] = i
+#nums = list(map(int, input("Enter numbers separated by spaces: ").split()))
+#target = int(input("Enter target: "))
+nums = [2,3,4,5,6]
+target = 7
+obj = Solution()
+print(obj.twoSum(nums, target))
